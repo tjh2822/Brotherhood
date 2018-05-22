e47f3db5a76f0e91caa8705152b18089876b9795
Thomas J. Hall <tjh2822@g.rit.edu>
2018-05-22 17:12:22 -0500

    started adding styling to page.php, added navbar

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
-	Brotherhood 1.0
+	Brotherhood 1.1
 	
 	Created by tjh2822.
-	Version 1.0
+	Version 1.1
 	
 	- - - - - - - - - - - - - - - - - - - - - - -
 
--- page.php
+++ page.php
@@ -19,11 +19,31 @@
 <div class="content">
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	
-		<h2>
-			<?php the_title(); ?>
-		</h2>
-		<?php the_content(); ?>
-		<?php comments_template( '', true ); ?>
+		<div class="page_header">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<h1>
+							<?php the_title(); ?>
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+
+			<div class="row">
+				<div class="col-sm-9">
+					<?php the_content(); ?>
+					<?php comments_template( '', true ); ?>
+				</div>
+				<div class="col-sm-3">
+					<p> TODO -> Put content here </p>
+				</div>
+			</div>		
+		</div>
+		
 	
 	<?php endwhile; ?>
 </div>
