3c48cfbe43604f8ff29320bc1b4db4f89732d4cc
Thomas J. Hall <tjh2822@g.rit.edu>
2018-05-22 23:20:00 -0500

    added footer

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,4 +1,43 @@
 	
 <div class="footer">
-	
+	<div class = "footer_top"></div>
+	<div class="footer_links">
+		<div class="row">
+	  		<div class="col-sm-3">
+	  			<h4>Northeast Region, Order of the Arrow</h4>
+	  			<br>
+
+	  		</div>
+	  		<div class="col-sm-3">
+	  			<h4>Programs</h4>
+	  			<br>
+	  			<a href="/programs/nls">National Leadership Seminar</a>
+	  			<br>
+	  			<a href="/programs/dylc">Developing Youth Leadership Conference</a>
+	  			<br>
+	  			<a href="/programs/oaha">Order of the Arrow, High Adventure</a>
+	  		</div>
+	  		<div class="col-sm-3">
+	  			<h4>Social Media</h4>
+	  			<br>
+	  		</div>
+	  		<div class="col-sm-3">
+	  			<h4>Contact</h4>
+	  			<br>
+	  			<a href="mailto:webmaster@northeast.oa-bsa.org">webmaster@northeast.oa-bsa.org</a>
+	  		</div>
+		</div>
+	</div>
+	<div class="footer_copyright">
+		<center>
+			<?php
+			$a = "© ";
+			$a .= date("Y"); 
+
+			$a .= " Copyright ";
+			print $a;
+			?>
+			<a href="https://northeast.oa-bsa.org/">Northeast Region, Order of the Arrow</a>
+		</center>
+	</div> 	
 </div>
